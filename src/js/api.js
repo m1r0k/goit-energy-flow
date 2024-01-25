ecb9a4c9f0437b0c67b39f03c43abc624d82e4ec
MaxKov86 <max.kov86@ukr.net>
2024-01-25 23:26:33 +0200

    Update exercises.html and exercises.css, add exercises.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,57 +1,60 @@
-import axios from "axios"
+import axios from 'axios';
 
 const BASE_URL = 'https://energyflow.b.goit.study/api/';
 
-
 // фільтри для блоку з видами вправав
 export function filterExercises(value) {
-    axios(`${BASE_URL}filters`, {
-        method: 'get',
-        params: {
-            filter: value,
-            limit: 12,
-        }
-    });
+  axios(`${BASE_URL}filters`, {
+    method: 'get',
+    params: {
+      filter: value,
+      limit: 12,
+    },
+  });
 }
 
 // отримати вправи
 export function getExercises(bodyparts, muscles, equipment, keyword) {
-    axios(`${BASE_URL}exercises`, {
-        method: 'get',
-        params: {
-            bodyparts,
-            muscles,
-            equipment,
-            keyword,
-            limit: 9,
-        }
-    });
+  axios(`${BASE_URL}exercises`, {
+    method: 'get',
+    params: {
+      bodyparts,
+      muscles,
+      equipment,
+      keyword,
+      limit: 9,
+    },
+  });
 }
 
 // взяти одину вправу за айді
 export function getExercise(id) {
-    axios(`${BASE_URL}exercises`, {
-        method: 'get',
-        params: {
-            id
-        }
-    });
+  axios(`${BASE_URL}exercises`, {
+    method: 'get',
+    params: {
+      id,
+    },
+  });
 }
 
 // отримати цититу
 export function getQuote() {
-    axios(`${BASE_URL}quote`, {
-        method: 'get',
-    });
+  axios(`${BASE_URL}quote`, {
+    method: 'get',
+  });
 }
 
 // підписатися на розсилку
 export function subscribe(email) {
-    axios.post(`${BASE_URL}subscription`, {
-        email
-    }, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+  axios.post(
+    `${BASE_URL}subscription`,
+    {
+      email,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 }
