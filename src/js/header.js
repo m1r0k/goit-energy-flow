3a929647115f94b5a38d3204db7281111902829e
David-Ivanov <111747295+David-Ivanov@users.noreply.github.com>
2024-01-28 20:32:40 +0200

    Revert "some add"

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,1 @@
-openBtn.addEventListener("click", () => {
-  menu.classList.toggle("open");
-  document.body.style.overflow = "hidden";
-  closeBtn.addEventListener("click", closeModal);
-});
 
-
-
-const closeModal = () => {
-  menu.classList.toggle("open");
-  document.body.style.overflow = "scroll";
-  removeEventListener('click', closeBtn);
-}
