c43502802b40bc14d07d7c41e455ad009727cb2b
Ivanov David <ivanovdavid905@gmail.com>
2024-01-28 20:07:56 +0200

    some add

--- src/js/header.js
+++ src/js/header.js
@@ -2,12 +2,16 @@ const menu = document.querySelector('.mobile-backdrop');
 const openBtn = document.querySelector('.open-mobile-menu-btn');
 const closeBtn = document.querySelector('.mobile-menu-close-btn');
 
-openBtn.addEventListener("click", function () {
+openBtn.addEventListener("click", () => {
   menu.classList.toggle("open");
   document.body.style.overflow = "hidden";
+  closeBtn.addEventListener("click", closeModal);
 });
 
-closeBtn.addEventListener("click", function () {
+
+
+const closeModal = () => {
   menu.classList.toggle("open");
   document.body.style.overflow = "scroll";
-});
+  removeEventListener('click', closeBtn);
+}
