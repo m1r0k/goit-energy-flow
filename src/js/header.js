313d38b1b24858d2fd7319796da86384954ffd3c
David-Ivanov <111747295+David-Ivanov@users.noreply.github.com>
2024-01-28 19:27:39 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,1 @@
-const menu = document.querySelector('.mobile-backdrop');
-const openBtn = document.querySelector('.open-mobile-menu-btn');
-const closeBtn = document.querySelector('.mobile-menu-close-btn');
 
-openBtn.addEventListener("click", function () {
-  menu.classList.toggle("open");
-  document.body.style.overflow = "hidden";
-});
-
-closeBtn.addEventListener("click", function () {
-  menu.classList.toggle("open");
-  document.body.style.overflow = "scroll";
-});
