61e63d46d6157a609ec1b63daa69076ef63989ba
Ivanov David <ivanovdavid905@gmail.com>
2024-01-28 21:15:14 +0200

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -3,7 +3,7 @@ const openBtn = document.querySelector('.open-mobile-menu-btn');
 const closeBtn = document.querySelector('.mobile-menu-close-btn');
 
 openBtn.addEventListener("click", () => {
-  menu.classList.toggle("open");
+  menu.classList.toggle("hidden");
   document.body.style.overflow = "hidden";
   closeBtn.addEventListener("click", closeModal);
 });
@@ -11,7 +11,7 @@ openBtn.addEventListener("click", () => {
 
 
 const closeModal = () => {
-  menu.classList.toggle("open");
+  menu.classList.toggle("hidden");
   document.body.style.overflow = "scroll";
   removeEventListener('click', closeBtn);
 }
