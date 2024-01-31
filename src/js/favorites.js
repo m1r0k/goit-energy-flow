6e93abd108091706cc4c8342ca5660c0263cf805
Iryna Vishnevska <Vishnya322@gmail.com>
2024-01-31 19:15:58 +0200

    110min-7

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,9 +1,11 @@
-/*let favoritesList;
+import { renderExercise } from './modal';
+let exList;
+let favoritesList;
 let paginationButtons;
 
 const itemsPerPage = 6;
-if (window.location.pathname === './src/favorites.html') {
-  favoritesList = document.querySelector('.workout-item');
+if (window.location.pathname === '/favorites.html') {
+  favoritesList = document.querySelector('.favorites-list');
   paginationButtons = document.querySelectorAll(
     '.favorites-pagination-block button'
   );
@@ -21,7 +23,7 @@ function showPage(pageNumber) {
   const startIndex = (pageNumber - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
 
-  const items = favoritesList.querySelectorAll('.workout-list');
+  const items = favoritesList.querySelectorAll('.workout-card');
   items.forEach((item, index) => {
     if (index >= startIndex && index < endIndex) {
       item.style.display = 'block';
@@ -37,37 +39,115 @@ function showPage(pageNumber) {
       button.classList.remove('active-btn');
     }
   });
-}*/
+}
 
-let favoritesList;
-
-if (window.location.pathname === '/src/favorites.html') {
-  favoritesList = document.querySelector('.favorites-list');
+if (window.location.pathname === '/favorites.html') {
   displayFavorites();
 }
 
 function displayFavorites() {
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  
+  const messageInfoBlock = document.querySelector('.message-info');
+  if (favorites.length > 0) {
+    messageInfoBlock.style.display = 'none';
+  } else {
+    messageInfoBlock.style.display = 'block';
+  }
 
-  if (favorites.length === 0) {
+    if (favorites.length === 0) {
     favoritesList.innerHTML = `
-      <div class="message-info">
-        <div class="message-info-block">
-          <img class="message-info-svg" src="./images/favorites/dumbbell.jpg" alt="dumbbell" />
-          <p class="message-info-text">
-            It appears that you haven't added any exercises to your favorites yet. To get
-            started, you can add exercises that you like to your favorites for easier
-            access in the future.
-          </p>
-        </div>
+      <div class="message-info-block">
+        <img class="message-info-svg" src="./images/favorites/dumbbell.jpg" alt="dumbbell" />
+        <p class="message-info-text">
+          It appears that you haven't added any exercises to your favorites yet. To get
+          started, you can add exercises that you like to your favorites for easier
+          access in the future.
+        </p>
       </div>
     `;
   } else {
-
     favoritesList.innerHTML = favorites.map(exercise => `
       <li class="favorites-item">
-        <span>${exercise.name}</span>
+        <div class="workout-card-wrapper">
+          <div class="workout-card">
+            <div class="workout-header">
+              <div class="workout-header-wrapper">
+                <p class="workout-title" >Workout</p>
+                <button class="workout-trash-btn" id="workout-trash-btn" type="button">
+                  <svg class="workout-icon-trash" id="workout-icon-trash" width="16" height="16">
+                    <use href="./images/icons.svg#icon-trash"></use>
+                  </svg>
+                </button>
+                <p class="workout-rating" id="workout-rating">4.0</p>
+                <svg class="workout-rating-icon" id="workout-rating-icon" width="18" height="18">
+                  <use href="./images/icons.svg#icon-star"></use>
+                </svg>
+              </div>
+              <button class="workout-start-button" id="workout-start-button" type="button">Start
+                <svg class="workout-icon-start" id="workout-icon-start" width="14" height="14">
+                  <use href="./images/icons.svg#icon-arrow"></use>
+                </svg>
+              </button>
+            </div>
+            <div class="workout-name-wrapper">
+              <svg class="workout-icon-man" id="workout-icon-man" width="24" height="24">
+                <use href="./images/icons.svg#icon-man"></use>
+              </svg>
+              <p class="workout-name" id="workout-name">${exercise.name}</p>
+            </div>
+            <div class="workout-inform-wrapper">
+              <p class="workout-calories" id="workout-calories">Burned calories: <span class="number-calories" id="number-calories">312&#160;&#8260;&#160;3&#160;min</span></p>
+              <p class="workout-body-part" id="workout-body-part">Body part: <span class="body-part" id="body-part">Waist</span></p>
+              <p class="workout-target" id="workout-target">Target: <span class="target" id="target">Abs</span></p>
+            </div>
+          </div>
+        </div>
       </li>
     `).join('');
+    
+  }
+}
+/  Start  /   
+
+exList.addEventListener('click', onCardClick);
+
+function onCardClick(e) {
+  let exSubtype = e.target.dataset.name;
+  let exFilter = e.target.dataset.filter;
+
+  if (exFilter === 'bodyparts') {
+    exFilter = 'bodypart';
+  }
+
+  if (e.target.nodeName === 'UL') {
+    return;
+  }
+
+  exForm.classList.remove('visually-hidden');
+  span.classList.remove('visually-hidden');
+  secondSpan.textContent = exSubtype;
+
+  exList.innerHTML = '';
+  exPagination.innerHTML = '';
+
+  getExercisesCards(exFilter, exSubtype).then(
+    ({ data: { results, totalPages } }) => {
+      exList.insertAdjacentHTML('beforeend', renderCards(results));
+
+      const starBtn = document.querySelectorAll('.workout-start-button');
+      starBtn.forEach(btn =>
+        btn.addEventListener('click', () => {
+          renderExercise(btn.dataset.id);
+        })
+      );
+
+      renderPagBtn(totalPages);
+      exPagination.firstChild.classList.add('active-pag-btn');
+      exList.removeEventListener('click', onCardClick);
+    }
+  );
+  if (innerWidth >= 768 && innerWidth < 1440) {
+    exHeader.style.marginBottom = '55px';
   }
 }
